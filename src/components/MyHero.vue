1bceb2d9c2de7645fa7d1e8b14c72357c6012bb2
satrio <satrioraja2400@gmail.com>
2025-07-11 16:36:24 +0700

    feat: prepare for initial deployment

--- src/components/MyHero.vue
+++ src/components/MyHero.vue
@@ -4,12 +4,12 @@
     <h1 class="text-5xl font-bold text-gray-800 mb-4 animate-slide in-left" style="animation-delay: 200ms;"> 
     Halo, Saya <span class="text-blue-600">Satrio Raja Nabil Rizky</span>  </h1> 
     <p class="text-xl text-gray-600 mb-8 animate-slide-in-left"  style="animation-delay: 400ms;"> 
-    Frontend & Backend Web Developer Junior. 
+    Frontend & Backend Web Developer Junior.
     </p> 
     <a href="#kontak" class="bg-blue-600 text-white font-bold py-3  px-6 rounded-lg hover:bg-blue-700 transition-colors duration-300  inline-block animate-slide-in-left" style="animation-delay: 600ms;">  Hubungi Saya 
     </a> 
     </div> 
-    <div class="md:w-1/2 flex justify-center animate-slide-in-right">  <img src="https://via.placeholder.com/400" alt="Foto Profil"  class="w-80 h-80 rounded-full object-cover shadow-lg"> 
+    <div class="md:w-1/2 flex justify-center animate-slide-in-right">  <img src="@/assets/images/profile.jpg" alt="Foto Profil"  class="w-80 h-80 rounded-full object-cover shadow-lg"> 
     </div> 
     </section> 
    </template> 
